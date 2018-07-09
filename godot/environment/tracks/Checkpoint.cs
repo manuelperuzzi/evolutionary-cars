80c68d0737ddfcb7a3b46b5fe4f0f25007464bb4
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2018-07-09 10:29:19 +0200

    Added documentation for Main.cs and Checkpoint.cs

--- godot/environment/tracks/Checkpoint.cs
+++ godot/environment/tracks/Checkpoint.cs
@@ -1,7 +1,13 @@
 using Godot;
 
+/// <summary>
+/// Class representing a checkpoint: a fixed point in the track used to evaluate the cars genotype. More
+/// in detail, the set of checkpoint on the track helps to calculate the distance traveled by each car, hence
+/// the evaluation of their genotypes.
+/// </summary>
 public class Checkpoint : Node2D
 {
+    /// <value>The score associated to the checkpoint, used in the evaluation phase.</value>
     public double Score
     {
         get;
